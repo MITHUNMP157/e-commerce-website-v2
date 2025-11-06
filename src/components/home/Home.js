fdb9144b7882c8f3df133a47b7848b02ccc062be
Mithun-MP Dev <162953538+MITHUNMP157@users.noreply.github.com>
2025-11-06 23:05:12 +0530

    Update Home.js

--- src/components/home/Home.js
+++ src/components/home/Home.js
@@ -4,8 +4,7 @@ import logo from "./home-images/image1.jpg";
 const Home = () => {
   return (
     <div>
-      <img src={logo} alt="logo" style={{ width: "100%", height: "auto" }} />
-  Test
+      <img src={logo} alt="logo" style={{ width: "100%", height: "200px" }} />
     </div>
   );
 };
